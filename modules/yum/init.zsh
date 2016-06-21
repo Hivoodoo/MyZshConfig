6177e468b062434e7f4eacfa04801b19f6f5fb1b
scinart <akubeej@gmail.com>
2016-06-21 14:59:52 +0800

    load yum module

--- modules/yum/init.zsh
+++ modules/yum/init.zsh
@@ -15,13 +15,13 @@ fi
 # Aliases
 #
 
-alias yumc='sudo yum clean all'    # Cleans the cache.
-alias yumh='yum history'           # Displays history.
-alias yumi='sudo yum install'      # Installs package(s).
-alias yuml='yum list'              # Lists packages.
-alias yumL='yum list installed'    # Lists installed packages.
-alias yumq='yum info'              # Displays package information.
-alias yumr='sudo yum remove'       # Removes package(s).
-alias yums='yum search'            # Searches for a package.
-alias yumu='sudo yum update'       # Updates packages.
-alias yumU='sudo yum upgrade'      # Upgrades packages.
+# alias yumc='sudo yum clean all'    # Cleans the cache.
+# alias yumh='yum history'           # Displays history.
+alias agi='sudo yum install'      # Installs package(s).
+alias agl='yum list'              # Lists packages.
+alias agL='yum list installed'    # Lists installed packages.
+alias agv='yum info'              # Displays package information.
+alias agx='sudo yum remove'       # Removes package(s).
+alias ags='yum search'            # Searches for a package.
+alias agu='sudo yum update'       # Updates packages.
+alias agU='sudo yum upgrade'      # Upgrades packages.
