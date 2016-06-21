398df9b55dcac511a32cada517ca4c044dc8a262
scinart <akubeej@gmail.com>
2016-06-21 13:45:32 +0800

    update dpkg/init.zsh

--- modules/dpkg/init.zsh
+++ modules/dpkg/init.zsh
@@ -8,7 +8,7 @@
 #
 
 # Return if requirements are not found.
-if (( ! $+commands[dpkg] && ! $+commands[apt-get] )); then
+if (( ! $+commands[dpkg] && ! $+commands[apt] )); then
   return 1
 fi
 
@@ -17,41 +17,46 @@ fi
 #
 
 # Cleans the cache.
-alias debc='sudo apt-get clean && sudo apt-get autoclean'
+# alias debc='sudo apt-get clean && sudo apt-get autoclean'
 
 # Displays a file's package.
-alias debf='apt-file search --regexp'
+# alias debf='apt-file search --regexp'
 
 # Installs packages from repositories.
-alias debi='sudo apt-get install'
+alias agi='sudo apt install'
 
 # Installs packages from files.
-alias debI='sudo dpkg -i'
+# alias debI='sudo dpkg -i'
 
 # Displays package information.
-alias debq='apt-cache show'
+alias agv='apt show'
 
 # Updates the package lists.
-alias debu='sudo apt-get update'
+alias agu='sudo apt update'
 
 # Upgrades outdated packages.
-alias debU='sudo apt-get update && sudo apt-get dist-upgrade'
+alias agU='sudo apt dist-upgrade'
 
 # Removes packages.
-alias debx='sudo apt-get remove'
+alias agx='sudo apt remove'
+alias agX='sudo apt purge'
 
 # Removes packages, their configuration, and unneeded dependencies.
-alias debX='sudo apt-get remove --purge && sudo apt-get autoremove --purge'
+# alias debX='sudo apt-get remove --purge && sudo apt-get autoremove --purge'
 
 # Searches for packages.
 if (( $+commands[aptitude] )); then
   alias debs='aptitude -F "* %p -> %d \n(%v/%V)" --no-gui --disable-columns search'
 else
-  alias debs='apt-cache search'
+  alias debs='apt search'
 fi
+
+alias ags='apt search'
+
 
 # Creates a basic deb package.
 alias deb-build='time dpkg-buildpackage -rfakeroot -us -uc'
 
 # Removes all kernel images and headers, except for the ones in use.
-alias deb-kclean='sudo aptitude remove -P "?and(~i~nlinux-(ima|hea) ?not(~n`uname -r`))"'
+
+# alias deb-kclean='sudo aptitude remove -P "?and(~i~nlinux-(ima|hea) ?not(~n`uname -r`))"'
